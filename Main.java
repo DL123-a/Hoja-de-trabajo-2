bc5f3e635e0513dbece7eb405311eed109e7ea0e
DL123-a <dannylealgt@gmail.com>
2026-02-05 22:47:13 -0600

    Implementacion de pila e interfaz de la misma

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main (String[] args){
+
+        Stack<Integer> pila =  new VectorStack<>();
+    }
+    
+}
--- VectorStack.java
+++ VectorStack.java
@@ -1,3 +1,34 @@
-public class VectorStack {
+import java.util.Vector;
+
+public class VectorStack<T> implements Stack<T> {
+    private Vector<T> data;
+
+
+    public VectorStack(){
+        data = new Vector<>();
+    }
+
+    @Override
+    public void push(T element){
+        data.add(element);
+    }
+
+    @Override
+    public T pop(){
+        return data.remove(data.size()-1);
+    }
+
+    @Override
+    public T peek(){
+        return data.get(data.size()-1);
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+
+    
     
 }
